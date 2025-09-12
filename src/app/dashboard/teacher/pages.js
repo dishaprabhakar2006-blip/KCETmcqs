345d5d21bf8daae26b6fdef8a9f39db9807262e6
dishaprabhakar2006-blip <dishaprabhakar2006@gmail.com>
2025-09-12 16:26:05 +0530

    Deleted file to fix build error

--- src/app/dashboard/teacher/pages.js
+++
@@ -1,8 +0,0 @@
-export default function TeacherDashboard() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Welcome Teacher!</h1>
-      <p>Upload syllabus or manage quizzes here 📂</p>
-    </div>
-  );
-}
